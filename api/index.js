c7a0c4d9aad5ef5a7683125f107abfb2264743cd
zelearfox <fox@zlr.su>
2025-04-30 23:05:08 +0000

    fix2

--- api/index.js
+++ api/index.js
@@ -4,6 +4,10 @@ import { Server } from 'socket.io';
 import cors from 'cors';
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors());
